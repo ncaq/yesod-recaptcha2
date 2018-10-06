be979806bb012c2695862865a9234b591289b464
ALeX Kazik <git@kazik.de>
2018-10-06 11:20:22 +0200

    added: Invisible ReCaptcha support

--- src/Yesod/ReCaptcha2.hs
+++ src/Yesod/ReCaptcha2.hs
@@ -3,11 +3,17 @@
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TupleSections     #-}
 module Yesod.ReCaptcha2 
     ( YesodReCaptcha(..)
       -- * ReCaptcha V2
     , reCaptcha
     , mReCaptcha
+      -- * Invisible ReCaptcha
+      -- $invisibleReCaptcha
+    , reCaptchaInvisible
+    , mReCaptchaInvisible
+    , reCaptchaInvisibleForm
     ) where
 
 import           ClassyPrelude.Yesod
@@ -72,3 +78,59 @@ mReCaptcha = do
             , fvErrors = Nothing
             , fvRequired = True
             }
+
+-- $invisibleReCaptcha
+-- 
+-- The Invisible ReCaptcha is not as easy as the V2.
+--
+-- 1. Function to check the response: 'reCaptchaInvisible' or 'mReCaptchaInvisible'.
+--
+-- 2. Add the following to the code which creates the form:
+--
+--     > (reCaptchaFormId, reCaptchaWidget, reCaptchaButtonAttributes) <- reCaptchaInvisibleForm Nothing
+--
+-- 3. Add the id to the form, class and attributes to the button and the widget somewhere.
+--    Example:
+--
+--     @
+--     \<form \#\#{reCaptchaFormId} method=post action=@{route} enctype=#{enctype}\>
+--       ^{widget}
+--       ^{reCaptchaWidget}
+--     
+--       \<button .g-recaptcha *{reCaptchaButtonAttributes}\>
+--         Submit
+--     @
+
+-- | check for Applicative style form
+reCaptchaInvisible :: YesodReCaptcha site => AForm (HandlerFor site) ()
+reCaptchaInvisible = formToAForm ((,[]) <$> mReCaptchaInvisible)
+
+-- | check for Monadic style form
+mReCaptchaInvisible :: YesodReCaptcha site => MForm (HandlerFor site) (FormResult ())
+mReCaptchaInvisible = fst <$> mReCaptcha
+
+-- | generate all required parts (except the check) for a Invisible ReCaptcha
+reCaptchaInvisibleForm 
+    :: YesodReCaptcha site 
+    => Maybe Text -- ^ The id of the form, a new will be created when 'Nothing' is passed
+    -> Maybe Text -- ^ The javascript to call after a successful captcha, it has to submit the form, a simple one will be generated when 'Nothing' is passed
+    -> HandlerFor site (Text, WidgetFor site (), [(Text, Text)])
+reCaptchaInvisibleForm mIdent mScript = do
+    mReCaptchaLanguage <- reCaptchaLanguage
+    siteKey <- reCaptchaSiteKey
+    identForm <- maybe newIdent return mIdent
+    scriptName <- maybe (("reCaptchaOnSubmit_" <>) <$> newIdent) return mScript
+    let
+      widget = do
+        case mReCaptchaLanguage of
+          Nothing ->
+            addScriptRemote "https://www.google.com/recaptcha/api.js"
+          Just hl ->
+            addScriptRemote $ "https://www.google.com/recaptcha/api.js?hl=" <> hl
+        when (isNothing mScript) $
+          toWidgetHead [hamlet|<script>function #{scriptName}(token) { document.getElementById("#{identForm}").submit(); } |]
+    return
+      ( identForm
+      , widget
+      , [("data-sitekey", siteKey), ("data-callback", scriptName)]
+      )
